23e67fb9bbf8f467973bf17a619bee3344912db7
Srdjan Sin Jovanovic <mp4065@gmail.com>
2016-11-18 21:17:39 +0100

    refactoring helper_functions

--- app/Helpers/helper_functions.php
+++ app/Helpers/helper_functions.php
@@ -1,30 +1,4 @@
 <?php
-function moja($numberOfInputFields, $postArray = [])
-{
-    $collection = [];
-    $countPost = count($postArray);
-    if ($countPost > $numberOfInputFields) {
-        for ($i = 0; $i < $numberOfInputFields; $i++) {
-            $newArray = null;
-            $newArray[] = $postArray[$i];
-            for (
-                $k = $numberOfInputFields - 1; $k < $countPost; $k + $numberOfInputFields
-            ) {
-                $newArray[] = $postArray[$k];
-            }
-            $collection[$i] = $newArray;
-        }
-    } else {
-        for ($i = 0; $i < $numberOfInputFields; $i++) {
-            $newArray = null;
-            $newArray[] = $postArray[$i];
-            $collection[$i] = $newArray;
-        }
-    }
-    
-    return $collection;
-}
-
 function insertValuesFromPostRequestIntoArray($numberOfInputFields, $postArray = [])
 {
     $niz = [];
