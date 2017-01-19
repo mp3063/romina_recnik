d60e23a51fb92d617495192190a8e53ee6476a3a
SrdjanSin <mp4065@gmail.com>
2017-01-19 15:48:06 +0100

    promenjeni linkovi iz srpskog u engleski

--- resources/views/partials/navigation.blade.php
+++ resources/views/partials/navigation.blade.php
@@ -14,8 +14,8 @@
                     <li><a href="/">Home</a></li>
                     <li><a href="#" data-toggle="modal" data-target="#myModal1">New Dictionary</a></li>
                     <li><a href="#" data-toggle="modal" data-target="#myModal">Insert</a></li>
-                    <li><a href="/plata">Plata</a></li>
-                    <li><a href="/plata-baza">Plata baza</a></li>
+                    <li><a href="/plata">Salary</a></li>
+                    <li><a href="/plata-baza">Salary MySQL</a></li>
                 </ul>
                 <form action="{{URL::to('/search')}}" class="navbar-form navbar-left" role="search">
                     <div class="form-group">
